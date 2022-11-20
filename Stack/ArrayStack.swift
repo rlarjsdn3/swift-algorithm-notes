749bba6a23703daa8c4c31350cc30ce835da4da8
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-20 21:41:10 +0900

    Update ArrayStack.swift

--- Stack/ArrayStack.swift
+++ Stack/ArrayStack.swift
@@ -2,16 +2,19 @@ struct Stack<Element> {
     // 요소를 저장할 배열
     private var items: [Element] = []
     
+    // 시간 복잡도: O(1)
     // 스택이 비어있으면 true, 비어있지 않다면 false를 반환
     public var isEmpty: Bool {
         return items.isEmpty
     }
     
+    // 시간 복잡도: O(1)
     // 배열의 맨 뒤에 요소 삽입
     public mutating func push(_ element: Element) {
         items.append(element)
     }
     
+    // 시간 복잡도: O(1)
     // 배열의 맨 뒤의 요소 삭제
     public mutating func pop() -> Element? {
         guard !isEmpty else {
