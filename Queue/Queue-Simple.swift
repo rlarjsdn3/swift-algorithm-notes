f75301ab0ca64f0b936a2bfcd6829ebc75c71dde
Zizi_Kim <rlarjsdn3@naver.com>
2022-11-27 20:47:32 +0900

    Update Queue-Simple.swift

--- Queue/Queue-Simple.swift
+++ Queue/Queue-Simple.swift
@@ -1,5 +1,5 @@
-public struct Queue<Element> {
-    fileprivate var array: [Element] = []
+public struct Queue<T> {
+    fileprivate var array: [T] = []
     
     public var count: Int {
         return array.count
@@ -9,25 +9,25 @@ public struct Queue<Element> {
         return array.isEmpty
     }
     
-    public mutating func enqueue(_ element: Element) {
+    public mutating func enqueue(_ element: T) {
         array.append(element)
     }
     
     @discardableResult
-    public mutating func dequeue() -> Element? {
+    public mutating func dequeue() -> T? {
         guard !isEmpty else { return nil }
         
         return array.removeFirst()
     }
     
-    public var front: Element? {
+    public var front: T? {
         return array.first
     }
 }
 
 extension Queue: Sequence {
-    public func makeIterator() -> AnyIterator<Element> {
-        var curr: Queue<Element> = self
+    public func makeIterator() -> AnyIterator<T> {
+        var curr: Queue<T> = self
         return AnyIterator {
             curr.dequeue()
         }
